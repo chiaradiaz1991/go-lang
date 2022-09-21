f93fd64103dd2f6e83c225279803e12bfc455110
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2022-09-21 20:22:15 +0200

    exercises variables

---
+++ variables/01/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Make It Blue
+//
+//  1. Change `color` variable's value to "blue"
+//
+//  2. Print it
+//
+// EXPECTED OUTPUT
+//  blue
+// ---------------------------------------------------------
+
+func main() {
+	// UNCOMMENT THE CODE BELOW:
+
+	color := "green"
+
+	// ADD YOUR CODE BELOW:
+
+	color = "blue"
+	fmt.Println("color: ", color)
+}
---
+++ variables/06/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Multi Assign #2
+//
+//  1. Assign the correct values to the variables
+//     to match to the EXPECTED OUTPUT below
+//
+//  2. Print the variables
+//
+// HINT
+//  Use multiple Println calls to print each sentence.
+//
+// EXPECTED OUTPUT
+//  Air is good on Mars
+//  It's true
+//  It is 19.5 degrees
+// ---------------------------------------------------------
+
+func main() {
+	// UNCOMMENT THE CODE BELOW:
+
+	var (
+		planet string
+		isTrue bool
+		temp   float64
+	)
+
+	planet, isTrue, temp = "Mars", true, 19.5
+
+	fmt.Println("Air is good on ", planet)
+	fmt.Println("It's ", isTrue)
+	fmt.Println("It is ", temp, " degrees")
+
+	// ADD YOUR CODE BELOW
+}
